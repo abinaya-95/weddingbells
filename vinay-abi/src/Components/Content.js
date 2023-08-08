445dd0140fc59c1dfc27bc3a728c83e84440f329
abinaya-95 <75747926+abinaya-95@users.noreply.github.com>
2023-08-08 01:01:25 -0400

    Update Content.js

--- vinay-abi/src/Components/Content.js
+++ vinay-abi/src/Components/Content.js
@@ -1,1 +1,20 @@
+import './Content.css';
+import React from 'react';
 
+export default function Content(){
+    return(
+        <div>
+            <div>
+        <h1>Its Happening !!!</h1>
+        <h1>You are invited</h1>
+      </div>
+      <div>
+        <h2>Hi there !!! Lets play a fun game before we reveal you the details, c'mon why not ? </h2>
+      </div>
+      <div>
+        <h2> Its a memory game and the rules are simple </h2>
+        <h2> Click on the cards and match the right images to win the game.</h2>
+        </div>
+        </div>
+    )
+}
